ed7a359f78d960f5287408ea8d6c64bc5b708579
AUo959 <206913296+AUo959@users.noreply.github.com>
2025-06-25 17:29:07 +0000

    Implement geometric algebra utility, plugin system skeleton, and tests

---
+++ tests/test_geometric_algebra.py
@@ -0,0 +1,15 @@
+# test_geometric_algebra.py
+import pytest
+from modules.symbolic_core.geometric_algebra import GeometricAlgebra
+
+def test_blade_multiplication():
+    ga = GeometricAlgebra()
+    e1 = ga.blades['e1']
+    e2 = ga.blades['e2']
+    result = ga.mult(e1, e2)
+    assert str(result) == str(e1 * e2)
+
+def test_pretty():
+    ga = GeometricAlgebra()
+    e1 = ga.blades['e1']
+    assert ga.pretty(e1) == str(e1)
